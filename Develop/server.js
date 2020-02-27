974d99ba55c3a17ef0fe1e12a038ecd72797de61
Iggy420 <igneriilla@gmail.com>
2020-02-27 15:42:12 -0600

    set up the front en routes

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//connect to db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+// ROUTES
+//app.use(require(".routes/api.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+//Start the Server
+
+app.listen(PORT, function() {
+  console.log("App is listening on PORT" + PORT);
+});
